bb9409f2d412326696dfe3efae7ffbcc0fcce20c
贾统林 <2894863924@qq.com>
2020-06-26 23:53:24 +0800

    jtl

---
+++ src/views/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="login">这里是登录dsadasdsadasdadsadsadsa</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+#login{
+  height:100vh;
+  background-color:#344a5f
+}
+</style>
