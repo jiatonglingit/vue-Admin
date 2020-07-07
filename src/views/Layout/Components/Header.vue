be4886d43f094b3037dafd14aa3371790bb333f5
贾统林 <2894863924@qq.com>
2020-07-08 00:27:37 +0800

    dev

--- src/views/Layout/Components/Header.vue
+++ src/views/Layout/Components/Header.vue
@@ -1,5 +1,18 @@
 <template>
-  <div id="header-warp">头部</div>
+  <div id="header-warp">
+    <div class="pull-left header-icon">
+      <svg-icon iconClass="menu" class="menu" />
+    </div>
+    <div class="pull-right">
+      <div class="user-info pull-left">
+        <img src="../../../assets/images/face.jpg" alt />
+        666
+      </div>
+      <div class="header-icon pull-left">
+        <svg-icon iconClass="exit" class="exit" />
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
@@ -7,11 +20,33 @@ export default {};
 <style lang="scss" scoped>
 #header-warp {
   position: fixed;
-  left: $menuWidth;
+  left: 250px;
   top: 0;
   right: 0;
   height: 75px;
   background-color: #ffffff;
   -webkit-box-shadow: 0 3px 16px 0 rgba(0, 0, 0, 0.1);
+}
+.header-icon {
+  padding: 20px 32px;
+  svg {
+    margin-bottom: -15px;
+    font-size: 25px;
+    cursor: pointer;
+  }
+}
+.user-info {
+  height: 100%;
+  padding: 20px 32px;
+  border-right: 1px solid #ededed;
+  + .header-icon {
+    padding: 20px 32px;
+  }
+  img {
+    display: inline-block;
+    margin-bottom: -12px;
+    margin-right: 18px;
+    border-radius: 50px;
+  }
 }
 </style>
--- src/views/Layout/Components/Main.vue
+++ src/views/Layout/Components/Main.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="main-warp">
-    <router-view></router-view>
+    <div class="main-content">
+      <div class="content">
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -9,12 +13,27 @@ export default {};
 <style lang="scss" scoped>
 #main-warp {
   position: fixed;
+  // height: 100vh;
   left: $menuWidth;
   top: 75px;
   right: 0;
   bottom: 0;
   border: 30px solid #f7f7f7;
   border-bottom: none;
+  -webkit-box-sizing: border-box;
+}
+.main-content {
+  width: 100%;
+  height: 100%;
+  // padding-top: $layoutHeader + 30;
+  padding-right: 30px;
+  -webkit-box-sizing: border-box;
+}
+.content {
+  width: 100%;
+  height: 100%;
+  padding: 30px 30px 0 30px;
+  background-color: #fff;
   box-sizing: border-box;
 }
 </style>
